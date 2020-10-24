703b31d7e5cd111b82cc390f7ff6bd42d7d14343
Ujjwal Kumar <ujjwalkumar8577@gmail.com>
2020-10-24 09:25:28 +0530

    Code reformat

--- app/src/main/java/com/ujjwalkumar/qkart/ShoppinglistActivity.java
+++ app/src/main/java/com/ujjwalkumar/qkart/ShoppinglistActivity.java
@@ -18,7 +18,6 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
