7750ad2bea8c770c6b4553b10014ae9ca8a21749
mohammadBasher <65073966+mohammadBasher@users.noreply.github.com>
2020-11-01 17:56:47 +0530

    Update items.java

--- app/src/main/java/com/ujjwalkumar/qkart/helper/items.java
+++ app/src/main/java/com/ujjwalkumar/qkart/helper/items.java
@@ -1,5 +1,5 @@
 package com.ujjwalkumar.qkart.helper;
-
+//the items class to store the details of an item.
 public class items {
     private String detail;
     private String id;
@@ -8,7 +8,7 @@ public class items {
     private String price;
     private String sellerid;
     private String status;
-    
+    //constructor of the class items
     public items(String detail,String id,String mrp,String name,String price,String sellerid,String status){
         this.detail=detail;
         this.id=id;
@@ -18,59 +18,59 @@ public class items {
         this.sellerid=sellerid;
         this.status=status;
     }
-
+    //getter function for detail
     public String getDetail() {
         return detail;
     }
-
+    //setter function for detail
     public void setDetail(String detail) {
         this.detail = detail;
     }
-
+    //getter function for id
     public String getId() {
         return id;
     }
-
+    //setter function for id
     public void setId(String id) {
         this.id = id;
     }
-
+    //getter function for mrp
     public String getMrp() {
         return mrp;
     }
-
+    //setter function for mrp
     public void setMrp(String mrp) {
         this.mrp = mrp;
     }
-
+    //getter function for name
     public String getName() {
         return name;
     }
-
+    //setter function for name
     public void setName(String name) {
         this.name = name;
     }
-
+    //getter function for price
     public String getPrice() {
         return price;
     }
-
+    //setter function for price
     public void setPrice(String price) {
         this.price = price;
     }
-
+    //getter fuction for seller id
     public String getSellerid() {
         return sellerid;
     }
-
+    //setter function for seller id
     public void setSellerid(String sellerid) {
         this.sellerid = sellerid;
     }
-
+    //getter function for status
     public String getStatus() {
         return status;
     }
-
+    //setter function for status
     public void setStatus(String status) {
         this.status = status;
     }
